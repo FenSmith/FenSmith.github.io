ad39205bc464266784ad604d341f621f04ac63fb
tangwei <930071049@qq.com>
2018-04-03 17:43:57 +0800

    7

--- js/main.js
+++ js/main.js
@@ -9,21 +9,21 @@ Main website JavaScript
 if (!window.addEventListener || !window.history || !window.requestAnimationFrame || !document.getElementsByClassName) return;
 
 
-// enable service worker
-if ('serviceWorker' in navigator) {
-
-  // register service worker
-  navigator.serviceWorker.register('/service-worker.js');
-
-  // load script to populate offline page list
-  if (document.getElementById('cachedpagelist') && 'caches' in window) {
-    var scr = document.createElement('script');
-    scr.src = '/js/offlinepage.js';
-    scr.async = 1;
-    document.head.appendChild(scr);
-  }
-
-}
+// // enable service worker
+// if ('serviceWorker' in navigator) {
+//
+//   // register service worker
+//   navigator.serviceWorker.register('/service-worker.js');
+//
+//   // load script to populate offline page list
+//   if (document.getElementById('cachedpagelist') && 'caches' in window) {
+//     var scr = document.createElement('script');
+//     scr.src = '/js/offlinepage.js';
+//     scr.async = 1;
+//     document.head.appendChild(scr);
+//   }
+//
+// }
 
 
 // remove URL #target after navigation animation and page unload
